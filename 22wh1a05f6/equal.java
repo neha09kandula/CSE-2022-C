d8c9e09da0a60e0ee58b91d41bfbbf16918d52d8
Meghana-5f6 <147139391+Meghana-5f6@users.noreply.github.com>
2023-10-13 15:50:57 +0530

    Add files via upload

---
+++ 22wh1a05f6/equal.java
@@ -0,0 +1,10 @@
+package csec2022;
+
+public class equal {
+   public static void main(String[]args) {
+	   String str1="java";
+	   String str2="Java";
+	   System.out.println(str1.equals(str2));
+	   System.out.println(str1.equalsIgnoreCase(str2));
+   }
+}
---
+++ 22wh1a05f6/methods.java
@@ -0,0 +1,19 @@
+package csec2022;
+
+public class methods {
+	public static void main(String[]args) {
+		//charAt
+		String str1="java codes";
+		String str2="java script";
+		char m=str1.charAt(6);
+		System.out.println(m);
+		int n=str1.codePointAt(2);//codePointAt
+		System.out.println(n);
+		//compareTo
+		int x=str1.compareTo(str2);//codePointAt
+		System.out.println(x);
+		//getChars
+		
+	}
+
+}

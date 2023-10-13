5db36002a87d55f92e16f32574b0700f487bfd36
Vasudha22WH1A05G7 <147139394+Vasudha22WH1A05G7@users.noreply.github.com>
2023-10-13 15:46:08 +0530

    Add files via upload

---
+++ 22WH1A05G7/typecasting.java
@@ -0,0 +1,17 @@
+package javaprograms225g7;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		long b=a;
+		float c=a;
+		double d=a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}

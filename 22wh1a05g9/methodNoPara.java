6b18c1951fe1a9787a38fa68ff6cfbab2f0d9ae8
Sravya_22wh1a05g9 <130379309+22wh1a05g9@users.noreply.github.com>
2023-10-13 18:39:41 +0530

    Create methodNoPara.java

---
+++ 22wh1a05g9/methodNoPara.java
@@ -0,0 +1,12 @@
+public class implementmethod{
+public int implementmethod()
+{
+int x=5;
+int y=6;
+return (x+y);
+}
+public static void main(String args[]){
+implementmethod object=new implementmethod();
+System.out.println(object.implementmethod());
+}
+}

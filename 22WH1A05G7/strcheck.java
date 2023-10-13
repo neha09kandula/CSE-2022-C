e595e93062b90a33a8051374afd93a666512b08e
Vasudha22WH1A05G7 <147139394+Vasudha22WH1A05G7@users.noreply.github.com>
2023-10-13 15:51:43 +0530

    Add files via upload

---
+++ 22WH1A05G7/strcheck.java
@@ -0,0 +1,25 @@
+package javaprograms225g7;
+
+public class strcheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Vasudha";
+		String s1="VASUDHA";
+		String s2="Vasudha";
+		char ch=s.charAt(5);
+		System.out.println(ch);
+		System.out.println(s.indexOf("s"));
+		System.out.println(s.equals(s1));
+		System.out.println(s.equalsIgnoreCase(s1));
+		System.out.println(s.compareTo(s1));
+		System.out.println(s.compareTo(s2));
+		String name="java strings";
+		char[] dst= new char [6];
+		name.getChars(5, 9, dst, 0);
+		System.out.println(dst);
+		StringBuffer m =new StringBuffer(s);
+		System.out.println(m);
+	}
+
+}
---
+++ 22WH1A05G7/strings.java
@@ -0,0 +1,12 @@
+package javaprograms225g7;
+
+public class strings {
+
+	public static void main(String[] args) {
+		String s1="Vasudha";
+		String s2="Ungarala";
+		System.out.println(s1+" "+s2);
+		System.out.println(s1.concat(s2));
+		}
+
+}

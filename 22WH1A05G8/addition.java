f72eddacd07ddfa491f785630c3ce4041df1b9aa
Ayesha22WH1A05G8 <147140160+Ayesha22WH1A05G8@users.noreply.github.com>
2023-10-13 13:22:38 +0530

    Add files via upload

---
+++ 22WH1A05G8/addition.java
@@ -0,0 +1,14 @@
+public class addition
+{
+int x=2;
+int y=3;
+public int addition()
+{
+return(x+y);
+}
+public static void main(String args[])
+{
+	addition s=new addition();
+	System.out.println(s.addition());
+}
+}

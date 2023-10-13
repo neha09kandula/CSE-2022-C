3a5e94022bd3af0eaf573afe58dfeeba0ffe8aaa
Vasudha22WH1A05G7 <147139394+Vasudha22WH1A05G7@users.noreply.github.com>
2023-10-13 13:33:19 +0530

    Add files via upload

---
+++ 22WH1A05G7/main.java
@@ -0,0 +1,11 @@
+public class main{
+	int x;
+	public main()
+{
+	x=10;
+}
+public static void main(String args[]){
+	main obj = new main();
+	System.out.println(obj.x);
+}
+}
---
+++ 22WH1A05G7/standard.java
@@ -0,0 +1,7 @@
+public class standard{	
+
+public static void main(String args[]){
+	String name = "vasudha";
+	System.out.println(name.length());
+}
+}

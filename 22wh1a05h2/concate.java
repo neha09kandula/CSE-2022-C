5bef77f4b08dc05597aa42d57f656ffb1c322a2d
rishitha5h2 <147139363+rishitha5h2@users.noreply.github.com>
2023-10-13 15:43:54 +0530

    Add files via upload

---
+++ 22wh1a05h2/concate.java
@@ -0,0 +1,12 @@
+package javaprograms;
+
+public class concate {
+
+	public static void main(String[] args) {
+		String name = "John";
+		name = name.concat("Abhram");
+				
+		System.out.println(name);
+	}
+
+}
---
+++ 22wh1a05h2/immutable.java
@@ -0,0 +1,12 @@
+package javaprograms;
+
+public class immutable {
+
+	public static void main(String[] args) {
+	 String firstname = "Rishi";
+	 String lastname = "tha";
+	 System.out.println(firstname.concat(lastname));
+
+	}
+
+}

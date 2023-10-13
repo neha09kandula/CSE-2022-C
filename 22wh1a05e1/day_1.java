d6abcfca886e3414ffbdc51d0c058cb132c0fae1
Ravali22wh1a05e1 <147139267+Ravali22wh1a05e1@users.noreply.github.com>
2023-10-13 15:51:07 +0530

    Add files via upload

---
+++ 22wh1a05e1/day_1.java
@@ -0,0 +1,14 @@
+
+
+public class day_1 {
+	int x=5;
+	int y=10;
+
+	public static void main(String[] args) {
+		day_1 num1=new day_1();
+		day_1 num2=new day_1();
+		System.out.println(num1.x);
+		System.out.println(num2.y);
+	}
+
+}

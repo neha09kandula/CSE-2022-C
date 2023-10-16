7a6d2078854cef09253453c6846c5b884e2ce8e3
Vennela-22wh1a05h4 <147139072+Vennela-22wh1a05h4@users.noreply.github.com>
2023-10-16 07:33:21 +0530

    Add files via upload

---
+++ 22WH1A05H4/Converson.java
@@ -0,0 +1,16 @@
+class standard {
+public static void main(String args[])
+{
+ int x=5;
+ int y;
+double result= Math.sqrt(5);
+System.out.println(result);
+String text="vennela";
+System.out.println(text);
+System.out.println(text.length());
+System.out.println(text.toUpperCase());
+System.out.println(text.toLowerCase());
+
+}
+}
+
---
+++ 22WH1A05H4/Multiple variables.java
@@ -0,0 +1,12 @@
+class student {
+   int x=5;
+   int y=6;
+   float z=7;
+public static void main(String args[])
+{
+student obj1=new student();
+System.out.println(obj1.x);
+System.out.println(obj1.y);
+System.out.println(obj1.z);
+}
+}

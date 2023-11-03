67a6f77af311f7300d8cf30ade8440dea153df4f
Charanya-225h6 <147139110+Charanya-225h6@users.noreply.github.com>
2023-11-03 15:49:56 +0530

    Add files via upload

--- firstframejava.java
+++ firstframejava.java
@@ -1,35 +1,29 @@
-package cse22wh1a05d4;
+package frames;
 import java.awt.Component;
 import java.awt.Frame;
 import java.awt.Graphics;
-import java.awt.event.*;
-
 public class firstframejava {
+
 	public static void main(String[] args) {
-		Frame f=new Frame("My first Frame");
-		f.setSize(400,300);
-		f.add(new MyComponent());
-		f.setVisible(true);
-		
-		
-	}
-	static class MyComponent extends Component{
+				Frame f=new Frame("My first frame");
+				f.setSize(400,300);
+				f.setBounds(100,50,400,300);
+				f.add(new MyComponent());
+				f.setVisible(true);
+			}
+	static  class MyComponent extends Component{
+
+
 	public void paint(Graphics g) {
-		g.drawRect(100, 100, 100, 100);
-		//g.drawLine(50, 50, 20, 30);
-	    g.drawOval(100, 100, 100, 100);
-		//g.drawLine(FRAMEBITS, ERROR, ALLBITS, ABORT);
-		//g.drawArc(FRAMEBITS, ERROR, WIDTH, HEIGHT, ALLBITS, ABORT);
-		g.draw3DRect(200, 200, 200, 200, getFocusTraversalKeysEnabled());
-	    g.fillRect(100,100,100,100);
-	    g.fillOval(250, 240, 30,50);
-	    g.drawArc(190,190,150,200,200,150);
-	    g.drawRect(400, 100, 100, 100);
-	    g.fillRect(400,100,100,100);
-	    g.fillOval(310, 240, 30,50);
-	    
-	    
+		g.drawLine(10,20,30,40);
+		g.drawArc(30, 400, 150, 150, 150,200);
+		g.drawRect(50,60,320,400);
+		g.drawOval(50, 100, 200, 300);
+        g.drawRoundRect(90,310,210,270, 370, 157);
+        g.fillOval(200, 310, 250, 170);
+        
 	}
-		
 	}
-}
+	}
+
+

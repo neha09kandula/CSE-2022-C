e4df78b0e403483b85c476e927186d0a21f88ff7
Anjali28-10 <147139725+Anjali28-10@users.noreply.github.com>
2023-11-03 15:47:39 +0530

    Add files via upload

--- firstframejava.java
+++ firstframejava.java
@@ -1,55 +1,35 @@
-package Frames;
-import java.awt.Color;
+package cse22wh1a05d4;
 import java.awt.Component;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.*;
 
-public class firstframejava{
-
-	public static void main(String[]args)
-	{
-		//auto-generated method
-		Frame f= new Frame("My first frame");
+public class firstframejava {
+	public static void main(String[] args) {
+		Frame f=new Frame("My first Frame");
 		f.setSize(400,300);
 		f.add(new MyComponent());
 		f.setVisible(true);
-    }
-	static class MyComponent extends Component{
-	/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-	public void paint(Graphics g) {
-		g.drawRect(200,200,120,120);//creation
-		g.fillRect(200,200,120,120);
-		g.setColor(Color.pink);
 		
-		
-		g.drawOval(120,120,120,120);
-		g.fillOval(120,120,120,120);
-		g.setColor(Color.pink);
-		
-		//draw lines
-		//g.drawLine(0,0,100,90);
-		
-		//draw ellipses and circles
-		g.drawOval(250, 120, 120, 120);
-		g.fillOval(250, 120, 120, 120);
-		g.setColor(Color.pink);
-		
-		g.drawOval(250, 250, 120, 120);
-		g.fillOval(250, 250, 120, 120);
-		g.setColor(Color.pink);
-		
-		g.drawOval(120, 250, 120, 120);
-		g.fillOval(120, 250, 120, 120);
-		g.setColor(Color.pink);
-		
-		
-	
-	
 		
 	}
+	static class MyComponent extends Component{
+	public void paint(Graphics g) {
+		g.drawRect(100, 100, 100, 100);
+		//g.drawLine(50, 50, 20, 30);
+	    g.drawOval(100, 100, 100, 100);
+		//g.drawLine(FRAMEBITS, ERROR, ALLBITS, ABORT);
+		//g.drawArc(FRAMEBITS, ERROR, WIDTH, HEIGHT, ALLBITS, ABORT);
+		g.draw3DRect(200, 200, 200, 200, getFocusTraversalKeysEnabled());
+	    g.fillRect(100,100,100,100);
+	    g.fillOval(250, 240, 30,50);
+	    g.drawArc(190,190,150,200,200,150);
+	    g.drawRect(400, 100, 100, 100);
+	    g.fillRect(400,100,100,100);
+	    g.fillOval(310, 240, 30,50);
+	    
+	    
+	}
+		
 	}
 }

37e7e69b5983ff180862e33bb4cb46dbbb865550
srihitha225h9 <147139135+srihitha225h9@users.noreply.github.com>
2023-11-03 15:53:49 +0530

    Add files via upload

---
+++ 22WH1A05H9/pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class pattern
+
+{
+public static void main(String args[])
+{
+int i,j;
+Scanner input=new Scanner(System.in);
+System.out.println("enter the number");
+int n=input.nextInt();
+for(i=0;i<=n;i++)
+{
+for(j=0;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+input.close();
+}

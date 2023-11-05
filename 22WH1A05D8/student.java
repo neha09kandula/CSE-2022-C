26b68ac9a72c691cf940843c80c03f8fc0c4098c
Farhanatabassum <147139256+Farhanatabassum@users.noreply.github.com>
2023-11-05 10:49:48 +0530

    Add files via upload

--- 22WH1A05D8/student.java
+++ 22WH1A05D8/student.java
@@ -1,7 +1,22 @@
-public class student{
-   int x=5;
-public static void main(string[] args){
-    student obj1=new student();
-    System.out.print(obj1.x);
-}
+public class Student
+{
+    String name;
+    int yob;
+    public int check(){
+        return yob;
+    }
+	public static void main(String[] args) { 
+	    Student s1= new Student();
+		System.out.println(s1.yob);
+		s1.name="Fouziya";
+		s1.yob=2006;
+		System.out.println(s1.yob+" "+s1.name);
+		System.out.println(s1.check());
+		Student s2= new Student();
+		//System.out.println(s2.yob);
+		s2.name="Meraj";
+		s2.yob=2008;
+		System.out.println(s2.yob+" "+s2.name);
+		System.out.println(s2.check());
+	}
 }

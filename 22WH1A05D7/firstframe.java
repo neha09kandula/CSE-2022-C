0d4262cad674f5f16dada7f839eb6ee027452a7a
vedasreekonka <147120250+vedasreekonka-5d7@users.noreply.github.com>
2023-11-06 17:57:10 +0530

    Add files via upload

--- 22WH1A05D7/firstframe.java
+++ 22WH1A05D7/firstframe.java
@@ -1,32 +1,32 @@
 package frames;
+import java.awt.Color;
 import java.awt.Frame;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.Component;
-import java.awt.Event;
-public class firstframe {
+import java.awt.Graphics;
+public class firstframe{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Frame f= new Frame("MY FIRST FRAME");
-		f.setSize(400,300);
+		Frame f = new Frame("my first frame");
+		f.setSize(400, 300);
 		f.setBounds(100,50,500,300);
 		f.add(new MyComponent());
 		f.setVisible(true);
+
+		// TODO Auto-generated method stub
+
 	}
-	static class MyComponent extends Component
-	{
-		public void paint(Graphics g)
-		{
-			g.drawRect(20, 10, 100, 60);
-			g.drawLine(0, 0, 100, 10);
-			g.drawLine(0, 90, 100, 10);
-			g.drawLine(40, 25, 250, 80);
-			g.drawRect(10, 150, 60, 50);
-			g.fillRect(100, 150, 60, 50);
-			g.drawRoundRect(190, 150, 60, 50, 15,15);
-			g.fillRoundRect(280, 150, 60, 50, 30, 40);
-			g.drawOval(10, 250, 50, 50);
-			g.fillOval(90, 250, 75,50);
-		}
+	static class MyComponent extends Component{
+	public void paint(Graphics g) {
+		g.drawRect(500,90,200,150);
+		g.drawOval(300,1,150,100);
+		g.setColor(Color.blue);
+		g.drawLine(100,5,150,150);
+		g.setColor(Color.pink);
+		g.drawArc(20,10, 70, 70, 265, 50);
+		g.setColor(Color.green);
+		g.fillOval(10,20,90,80);
+		g.setColor(Color.yellow);
+		g.fillRect(150,10,70,50);
 	}
-	
+	}
 }

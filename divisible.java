8a95b710ff1a4760de92671a7cdc377d49e9987d
rishithaf7 <147139702+rishithaf7@users.noreply.github.com>
2023-11-10 15:12:09 +0530

    Add files via upload

--- divisible.java
+++ divisible.java
@@ -1,20 +1,18 @@
-
-import java.util.*;
-public class divisible
+import java.util;
+public class  div
 {
-public static void main(String args[])
-{
-	System.out.println(" enter the number");
-	Scanner input= new Scanner(System.in);
-	int number=input.nextInt();
-	if(number%5==0)
-	{
-		System.out.println("it is divisible by 5");
-	
-	}
-	else
-	{
-		System.out.println("it is not divisible by 5");
-	}
-}
+            public static void main(String args[])
+            {
+                       system.out.println("enter your number:");
+                       Scannar input=new Scannar(System.in);
+                       int num=input.nextInt();
+                       if(num/5==0)
+                       {
+                                  System.out.println("it is divisible by 5);
+                       }
+                       else
+                       {
+                                  System.out.println("it is not divisible by 5);
+                       }
+            }
 }
---
+++ labelframe.java
@@ -0,0 +1,9 @@
+package cse225f7;
+
+public class labelframe {
+
+	public labelframe() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

bbc7d2ba86006bd39665cb040f85d18bd7570161
joshitha5g4 <147138847+joshitha5g4@users.noreply.github.com>
2023-11-10 15:46:25 +0530

    Add files via upload

---
+++ 22WH1A05G4/addition.java
@@ -0,0 +1,12 @@
+public class addition{
+	public int addition()
+{
+	int x=5;
+	int y=3;
+	return x+y;
+}
+public static void main(String args[]){
+	addition obj = new addition();
+	System.out.println(obj.addition());
+}
+}

1baa1105af5c39827f097fe04d2684e88024ab5b
Pallavi_5i2 <130372618+PallaviGudupallavi@users.noreply.github.com>
2023-11-10 15:36:29 +0530

    Add files via upload

---
+++ 22WH1A05I2/appletes.java
@@ -0,0 +1,12 @@
+package Frames;
+import java.awt.Frame;
+
+public class firstframejava {
+	public static void main(String[]args) {
+		//auto-generated method
+		Frame f= new Frame("My first frame");
+		f.setSize(400,300);
+		f.setVisible(true);
+		
+	}
+}

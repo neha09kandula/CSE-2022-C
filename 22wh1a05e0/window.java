51046521a7511d0c652718cebd8a3f7563ff7819
AksharaBhavitha <147138993+AksharaBhavitha@users.noreply.github.com>
2023-11-10 15:58:30 +0530

    Add files via upload

---
+++ 22wh1a05e0/window.java
@@ -0,0 +1,35 @@
+package javaprograms;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class window extends Frame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	window(){
+		addWindowListener(new WindowAdapter()
+				{
+			public void windowClosing(WindowEvent e)
+			{
+				dispose();
+			}
+				});
+		setSize(400,400);
+		setBounds(30,60,80,120);
+		setLayout(null);
+		setVisible(true);
+	}
+	public void Paint(Graphics g)
+	{
+		g.drawRect(300,210,120,20);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      new window();
+	}
+
+}

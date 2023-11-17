ea4b9b688223de6519bd8b49227cda5e968d5615
MandalojuSrujana <140646035+MandalojuSrujana@users.noreply.github.com>
2023-11-17 15:56:41 +0530

    Create Calculator.java

---
+++ 22wh1a05e9/Calculator.java
@@ -0,0 +1,65 @@
+package Frames;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class grids extends WindowAdapter {
+    private Frame frame;
+
+    public grids() {
+    	frame = new Frame();
+        frame.setLayout(new BorderLayout()); // Use BorderLayout for the frame
+
+        TextField textField = new TextField();
+        textField.setPreferredSize(new Dimension(500, 50)); // Set preferred size for the TextField
+        frame.add(textField, BorderLayout.NORTH);
+
+        Panel buttonPanel = new Panel();
+        buttonPanel.setLayout(new GridLayout(4, 4, 5, 5));
+
+        // ... (Button initialization remains the same)
+        buttonPanel.add(new Button("1"));
+        buttonPanel.add(new Button("2"));
+        buttonPanel.add(new Button("3"));
+        buttonPanel.add(new Button("Del"));
+       
+       
+        buttonPanel.add(new Button("4"));
+        buttonPanel.add(new Button("5"));
+        buttonPanel.add(new Button("6"));
+        buttonPanel.add(new Button("Clr"));
+        
+        buttonPanel.add(new Button("7"));
+        buttonPanel.add(new Button("8"));
+        buttonPanel.add(new Button("9"));
+        buttonPanel.add(new Button("="));
+       
+        buttonPanel.add(new Button("0"));
+        buttonPanel.add(new Button("*"));
+        buttonPanel.add(new Button("+"));
+        buttonPanel.add(new Button("-"));
+        
+        frame.add(buttonPanel, BorderLayout.CENTER);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new grids();
+    }
+}
+
+        
+      
+      
+   
+         
+      
+
+    

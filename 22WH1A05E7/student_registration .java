c2170440b1e4b1d475d6d117b9291c1bf86c1c88
madhavi22wh1a05e7 <147139035+madhavi22wh1a05e7@users.noreply.github.com>
2023-11-24 13:23:52 +0530

    Create student_registration .java

---
+++ 22WH1A05E7/student_registration .java
@@ -0,0 +1,29 @@
+package madhavie7;
+	import javax.swing.JFrame;
+	 
+	public class student_registration extends JFrame {
+	 
+	    public student_registration() {
+	 
+	    }
+	 
+	    public void setBounds() {
+	 
+	    }
+	 
+	    public void addComponents() {
+	 
+	    }
+	 
+	    public static void main(String[] args) {
+	    	student_registration frame = new student_registration();
+	        frame.setTitle("Student Register Form");
+	        frame.setVisible(true);
+	        frame.setBounds(500, 100, 500, 700);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setResizable(true);
+	 
+	    }
+	 
+	}
+

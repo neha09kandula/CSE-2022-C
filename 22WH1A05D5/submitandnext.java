00ff757876ad7aa07a63e08bc081b6b395e51b21
veda5d5 <140432451+veda-2081@users.noreply.github.com>
2023-11-24 14:40:20 +0530

    Add files via upload

---
+++ 22WH1A05D5/submitandnext.java
@@ -0,0 +1,40 @@
+package frames;
+import java.awt.*;
+import java.awt.event.*;
+
+	public class submitandnext extends Frame {
+	    submitandnext() {
+	        Frame f = new Frame("HelloFrame"); 
+	        f.setSize(400, 400);
+	        f.setLayout(null); 
+	        f.setVisible(true);
+
+	        Button a = new Button("submit");
+	        Button b = new Button("next");
+	        
+	        f.add(a);
+	        f.add(b);
+	        a.setBounds(190, 200, 80, 50);
+	        b.setBounds(80, 200, 80, 50); 
+	        
+	        a.setBackground(Color.red);
+	        b.setBackground(Color.green);
+	        
+
+	        b.addActionListener(new ActionListener() { 
+	            public void actionPerformed(ActionEvent e) {
+	                
+	            }
+	        });
+
+	        f.addWindowListener(new WindowAdapter() {
+	            public void windowClosing(WindowEvent e) {
+	                f.dispose();
+	            }
+	        });
+	    }
+
+	    public static void main(String[] args) {
+	        new submitandnext(); 
+	    }
+	}

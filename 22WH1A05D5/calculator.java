bd417536bb90f2a1d40c71e6ea6d964de58e6e08
veda5d5 <140432451+veda-2081@users.noreply.github.com>
2023-11-24 14:39:24 +0530

    Update calculator.java

--- 22WH1A05D5/calculator.java
+++ 22WH1A05D5/calculator.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 
 public class calculator extends Frame {
     TextField enterField ;
-    Button submitButton,stopButton,aButton,bButton,cButton,dButton,eButton,fButton,gButton,hButton,iButton,jButton,kButton;
+    Button aButton,bButton,cButton,dButton,eButton,fButton,gButton,hButton,iButton,jButton,kButton,lButton,mButton,nButton,oButton,pButton,qButton,rButton,sButton,tButton,uButton;
 
     public calculator() {
         setTitle("Calculator");
@@ -13,45 +13,75 @@ public class calculator extends Frame {
         Label enterLabel = new Label("Enter");
         
         enterField = new TextField();
-        submitButton = new Button("Submit");
-        stopButton = new Button("Stop");
-        aButton = new Button("1");
-        bButton = new Button("2");
-        cButton = new Button("3");
-        dButton = new Button("4");
-        eButton = new Button("5");
-        fButton = new Button("6");
-        gButton = new Button("7");
-        hButton = new Button("8");
-        iButton = new Button("9");
-        jButton = new Button("0");
-        kButton = new Button("=");
-        
+        aButton = new Button("7");
+        bButton = new Button("8");
+        cButton = new Button("9");
+        dButton = new Button("DEL");
+        eButton = new Button("AC");
+        fButton = new Button("4");
+        gButton = new Button("5");
+        hButton = new Button("6");
+        iButton = new Button("*");
+        jButton = new Button("/");
+        kButton = new Button("1");
+        lButton = new Button("2");
+        mButton = new Button("3");
+        nButton = new Button("+");
+        oButton = new Button("-");
+        pButton = new Button("0");
+        qButton = new Button(".");
+        rButton = new Button("*10^x");
+        sButton = new Button("ANS");
+        tButton = new Button("=");
         
         enterLabel.setBounds(50,30,80,20);
         
-        enterField.setBounds(50, 50, 150, 30);
+        enterField.setBounds(50, 50, 190, 30);
         
         aButton.setBounds(50, 100, 30, 30);
         bButton.setBounds(90, 100, 30, 30);
         cButton.setBounds(130, 100, 30, 30);
         dButton.setBounds(170, 100, 30, 30);
-        eButton.setBounds(50, 130, 30, 30);
-        fButton.setBounds(90, 130, 30, 30);
-        gButton.setBounds(130, 130, 30, 30);
-        hButton.setBounds(170, 130, 30, 30);
-        iButton.setBounds(50, 160, 30, 30);
-        jButton.setBounds(90, 160, 30, 30);
-        kButton.setBounds(130, 160, 70, 30);
+        eButton.setBounds(210, 100, 30, 30);
+        fButton.setBounds(50, 130, 30, 30);
+        gButton.setBounds(90, 130, 30, 30);
+        hButton.setBounds(130, 130, 30, 30);
+        iButton.setBounds(170, 130, 30, 30);
+        jButton.setBounds(210, 130, 30, 30);
+        kButton.setBounds(50, 160, 30, 30);
+        lButton.setBounds(90, 160, 30, 30);
+        mButton.setBounds(130, 160, 30, 30);
+        nButton.setBounds(170, 160, 30, 30);
+        oButton.setBounds(210, 160, 30, 30);
+        pButton.setBounds(50, 190, 30, 30);
+        qButton.setBounds(90, 190, 30, 30);
+        rButton.setBounds(130, 190, 30, 30);
+        sButton.setBounds(170, 190, 30, 30);
+        tButton.setBounds(210, 190, 30, 30);
         
-        submitButton.setBounds(50, 220, 80, 30);
-        stopButton.setBounds(130, 220, 80, 30);
-        
-        
+        aButton.setBackground(Color.LIGHT_GRAY);
+        bButton.setBackground(Color.LIGHT_GRAY);
+        cButton.setBackground(Color.LIGHT_GRAY);        
+        dButton.setBackground(Color.pink);
+        eButton.setBackground(Color.pink);
+        fButton.setBackground(Color.LIGHT_GRAY);
+        gButton.setBackground(Color.LIGHT_GRAY);
+        hButton.setBackground(Color.LIGHT_GRAY);
+        iButton.setBackground(Color.pink);       
+        jButton.setBackground(Color.pink);
+        kButton.setBackground(Color.LIGHT_GRAY);
+        lButton.setBackground(Color.LIGHT_GRAY);
+        mButton.setBackground(Color.LIGHT_GRAY);
+        nButton.setBackground(Color.pink);
+        oButton.setBackground(Color.pink);
+        pButton.setBackground(Color.LIGHT_GRAY);
+        qButton.setBackground(Color.LIGHT_GRAY);
+        rButton.setBackground(Color.LIGHT_GRAY);
+        sButton.setBackground(Color.pink);
+        tButton.setBackground(Color.pink);       
+   
         add(enterLabel);	
         add(enterField);
-        add(submitButton);
-        add(stopButton);
         add(aButton);
         add(bButton);
         add(cButton);
@@ -63,9 +93,17 @@ public class calculator extends Frame {
         add(iButton);
         add(jButton);
         add(kButton);
+        add(lButton);
+        add(mButton);
+        add(nButton);
+        add(oButton);
+        add(pButton);
+        add(qButton);
+        add(rButton);
+        add(sButton);
+        add(tButton);
         
-        
-        submitButton.addActionListener(new ActionListener() {
+        tButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             }
         });

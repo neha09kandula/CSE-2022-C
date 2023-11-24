900376faef664d8149f6761cf2eea8bd7e72a5f7
veda5d5 <140432451+veda-2081@users.noreply.github.com>
2023-11-24 22:55:12 +0530

    Update StudentRegistration.java

--- 22WH1A05D5/StudentRegistration.java
+++ 22WH1A05D5/StudentRegistration.java
@@ -1,111 +1,74 @@
 package frames;
-import java.awt.*;
-import java.awt.event.*;
 
-public class calculator extends Frame {
-    TextField enterField ;
-    Button aButton,bButton,cButton,dButton,eButton,fButton,gButton,hButton,iButton,jButton,kButton,lButton,mButton,nButton,oButton,pButton,qButton,rButton,sButton,tButton,uButton;
+//importing the necessary libraries  
+import java.awt.*;    
+import java.awt.event.*;    
 
-    public calculator() {
-        setTitle("Calculator");
-        setSize(400, 400);
-        setLayout(null);
-        Label enterLabel = new Label("Enter");
-        
-        enterField = new TextField();
-        aButton = new Button("7");
-        bButton = new Button("8");
-        cButton = new Button("9");
-        dButton = new Button("DEL");
-        eButton = new Button("AC");
-        fButton = new Button("4");
-        gButton = new Button("5");
-        hButton = new Button("6");
-        iButton = new Button("*");
-        jButton = new Button("/");
-        kButton = new Button("1");
-        lButton = new Button("2");
-        mButton = new Button("3");
-        nButton = new Button("+");
-        oButton = new Button("-");
-        pButton = new Button("0");
-        qButton = new Button(".");
-        rButton = new Button("*10^x");
-        sButton = new Button("ANS");
-        tButton = new Button("=");
-        
-        enterLabel.setBounds(50,30,80,20);
-        
-        enterField.setBounds(50, 50, 190, 30);
-        
-        aButton.setBounds(50, 100, 30, 30);
-        bButton.setBounds(90, 100, 30, 30);
-        cButton.setBounds(130, 100, 30, 30);
-        dButton.setBounds(170, 100, 30, 30);
-        eButton.setBounds(210, 100, 30, 30);
-        fButton.setBounds(50, 130, 30, 30);
-        gButton.setBounds(90, 130, 30, 30);
-        hButton.setBounds(130, 130, 30, 30);
-        iButton.setBounds(170, 130, 30, 30);
-        jButton.setBounds(210, 130, 30, 30);
-        kButton.setBounds(50, 160, 30, 30);
-        lButton.setBounds(90, 160, 30, 30);
-        mButton.setBounds(130, 160, 30, 30);
-        nButton.setBounds(170, 160, 30, 30);
-        oButton.setBounds(210, 160, 30, 30);
-        pButton.setBounds(50, 190, 30, 30);
-        qButton.setBounds(90, 190, 30, 30);
-        rButton.setBounds(130, 190, 30, 30);
-        sButton.setBounds(170, 190, 30, 30);
-        tButton.setBounds(210, 190, 30, 30);
-        
-        
-        dButton.setBackground(Color.pink);
-        eButton.setBackground(Color.pink);
-        iButton.setBackground(Color.pink);       
-        jButton.setBackground(Color.pink);
-        nButton.setBackground(Color.pink);
-        oButton.setBackground(Color.pink);
-        sButton.setBackground(Color.pink);
-        tButton.setBackground(Color.pink);       
-   
-        add(enterLabel);	
-        add(enterField);
-        add(aButton);
-        add(bButton);
-        add(cButton);
-        add(dButton);
-        add(eButton);
-        add(fButton);
-        add(gButton);
-        add(hButton);
-        add(iButton);
-        add(jButton);
-        add(kButton);
-        add(lButton);
-        add(mButton);
-        add(nButton);
-        add(oButton);
-        add(pButton);
-        add(qButton);
-        add(rButton);
-        add(sButton);
-        add(tButton);
-        
-        tButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            }
-        });
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
-        });
+//class which inherits the WindowAdapter class  
+public class studentregistration extends WindowAdapter {
+	TextField nameField, emailField,mobile_number,password;
+//object of Frame  
+ Frame f;    
+//class constructor  
+ studentregistration() {    
+//creating the frame  
+     f = new Frame("my first frame");
+     Label nameLabel=new Label("Name :");
+//adding  WindowListener to the frame  
+     f.addWindowListener (this);
+     nameLabel.setBounds(150,100,200,30);
+     nameField=new TextField();
+     nameField.setBounds(250,100,200,30);
+     f.add(nameField);
+     f.add(nameLabel);
+     
+     
+     emailField=new TextField();
+     emailField.setBounds(250,200,200,30);
+     f.add(emailField);
+     
+     Label emailLabel=new Label("email :");
+     emailLabel.setBounds(150,200,200,30);
+     f.add(emailLabel);
+     
+     mobile_number=new TextField();
+     mobile_number.setBounds(250,300,200,30);
+     f.add(mobile_number);
+     
+     Label mobile_number=new Label("mobile number :");
+     mobile_number.setBounds(150,300,200,30);
+     f.add(mobile_number);
+     
+     password=new TextField();
+     password.setBounds(250,400,200,30);
+     f.add(password);
+     
+     Label password=new Label("password :");
+     password.setBounds(150,400,200,30);
+     f.add(password);
+     
+     Button b=new Button("reset");
+     f.add(b);
+     b.setBounds(150,500,100,50);
+     Button c=new Button("register");
+     f.add(c);
+     c.setBounds(450,500,100,50);
+     Button d=new Button("back");
+     f.add(d);
+     d.setBounds(300,600,100,50);
+     
+//setting the size, layout and visibility of frame  
+     f.setSize (700, 700);    
+     f.setLayout (null);    
+     f.setVisible (true);    
+ }    
 
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new calculator();
-    }
-}
+//overriding the windowClosing() method   
+public void windowClosing (WindowEvent e) {    
+ f.dispose();    
+}    
+//main method  
+public static void main(String[] args) {    
+ new studentregistration();    
+}    
+}  

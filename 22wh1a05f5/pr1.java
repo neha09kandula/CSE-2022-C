19ed321dabf2c45f45f430272301ef7e11022606
chaitanya-5f5 <147139319+chaitanya-5f5@users.noreply.github.com>
2023-12-08 15:05:32 +0530

    Update pr1.java

--- 22wh1a05f5/pr1.java
+++ 22wh1a05f5/pr1.java
@@ -1,1 +1,49 @@
+package f5pack;
+import java.awt.*;
+import java.awt.event.*;
+public class pr1 extends Frame implements ActionListener {
+    Button b1, b2, b3;
+    pr1() {
+        setSize(800, 800);
+        setVisible(true);
+        setLayout(null);
+        setTitle("F1");
+        setBackground(Color.darkGray);
+        b1 = new Button("NEXT");
+        b1.setBackground(Color.YELLOW);
+        b2 = new Button("CLICK");
+        b2.setBackground(Color.YELLOW);
+        b1.setBounds(250, 300, 100, 50);
+        b2.setBounds(500, 300, 100, 50);
+        add(b1);
+        add(b2);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });}
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == b1) {
+            Frame f = new Frame();
+            f.setSize(800, 800);
+            f.setVisible(true);
+            f.setLayout(null);
+            f.setTitle("F2");
+            b3 = new Button("WELCOME");
+            b3.setBounds(270, 300, 100, 50);
+            f.add(b3);
+            f.setBackground(Color.PINK);
+            f.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent e) {
+                    f.dispose();
+                }
+            });           
+        } else if (ae.getSource() == b2) {
+            System.exit(0);
+        }}
+    public static void main(String[] args) {
+        new pr1();        
+    }}
 

b6f2d2a04f3dc8d2ad1855920e89ef0381ca0cec
Saraswathi22wh1a05d2 <147139210+Saraswathi22wh1a05d2@users.noreply.github.com>
2023-12-15 15:47:01 +0530

    Add files via upload

---
+++ 22wh1a05d2/method_over_loading.java
@@ -0,0 +1,19 @@
+package cse225d2;
+
+public class method_over_loading {
+public int sum1(int x,int y){
+return(x+y);
+}
+public int sum1(int x,int y,int z){
+return(x+y+z);
+}
+public int sum1(double x,double y){
+return(x+y);
+}
+public static void main(String args[]){
+sum1 s=new sum1();
+System.out.println(s.sum1(6,4));
+System.out.println(s.sum1(6,4,2));
+System.out.println(s.sum1(6,4));
+}
+}

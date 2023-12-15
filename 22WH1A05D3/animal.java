013dcef877b7d8f8920bb9195a8da87e9e463db1
22wh1a05d3 <147804868+22wh1a05d3@users.noreply.github.com>
2023-12-15 14:08:15 +0530

    Add files via upload

---
+++ 22WH1A05D3/animal.java
@@ -0,0 +1,37 @@
+package Java22wh1a05d3;
+
+//single inheritance
+ public class animal{
+	public static void main (String args[])
+	{
+		
+		Dog dg  = new Dog();
+		dg.colour="brown";
+		dg.typefood="milk";
+		dg.bread="shepard" ;
+		dg.method1();
+		dg.method2();
+		System.out.print(" colour is "   +  dg.colour);
+		System.out.print(" ");
+		System.out.print(" bread is "  +   dg.bread);
+		System.out.print(" ");
+		System.out.print(" typefood is "  +   dg.typefood);
+	}
+
+}
+class Animal1{
+	String colour;
+	String typefood;
+	void method1()
+	{
+		System.out.println("in super calss");
+	}
+}
+class Dog extends Animal1{
+	String bread;
+	void method2()
+	{
+		System.out.println("in sub calss");
+	}
+	
+}

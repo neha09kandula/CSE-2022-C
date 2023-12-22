1501b13951d14947aecb630d0b56c766b6cd906f
rishithaf7 <147139702+rishithaf7@users.noreply.github.com>
2023-12-22 15:48:44 +0530

    Add files via upload

---
+++ 22wh1a05f7/stringconc.java
@@ -0,0 +1,9 @@
+package cse225f7;
+
+public class stringconc {
+		 public static void main(String args[]){  
+		   String s="Sachin"+" Tendulkar";  
+		   System.out.println(s); 
+		 }  
+		}  
+	    

a79e0790b0148713935e8f3f8e24c32c9563c377
Sravya_22wh1a05g9 <130379309+22wh1a05g9@users.noreply.github.com>
2023-12-22 14:27:12 +0530

    Create LabInternalB.java

---
+++ 22wh1a05g9/LabInternalB.java
@@ -0,0 +1,15 @@
+package LabInternal;
+
+public class stringconcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="Hello";
+		String s2="World";
+		System.out.println(s1.concat(s2));
+		System.out.println(s1+" "+s2);
+		
+
+	}
+
+}

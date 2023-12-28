26a7946475688b4e8f67e5ace37b1117ebbe80f7
srujana225h8 <147139729+srujana225h8@users.noreply.github.com>
2023-12-28 11:35:52 +0530

    Create VehicleTest.java

---
+++ 22wh1a05h8/VehicleTest.java
@@ -0,0 +1,28 @@
+
+
+class Vehicle {
+
+ public void drive() {
+     System.out.println("Driving a vehicle");
+ }
+}
+
+class Car extends Vehicle {
+
+ public void drive() {
+     System.out.println("Repairing a car");
+ }
+}
+
+public class VehicleTest {
+ public static void main(String[] args) {
+ 
+     Vehicle vehicle = new Vehicle();
+         
+     vehicle.drive();
+   
+     Car car = new Car();
+    
+     car.drive();
+ }
+}

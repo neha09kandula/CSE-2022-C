1f98f4b7ac13140f48d1bc1bb6d41f0e6ab3a00d
shynitha-5i6 <147139306+shynitha-5i6@users.noreply.github.com>
2023-12-28 12:00:28 +0530

    Add files via upload

---
+++ 22wh1a05i6/Animals.java
@@ -0,0 +1,34 @@
+package Frame;
+
+abstract public class Animals 
+{
+		 void cats() {
+			 System.out.println("y");
+		 }
+		 void dogs() {
+			 System.out.println("x");
+		 }
+		}
+		class Cats extends Animals{
+			void cats() {
+				System.out.println(" cats Meow");
+			}
+			
+		}
+		class Dogs extends Animals{
+		   void dogs() {
+			   System.out.println("Dogs Bark");
+		   }
+		   
+		}
+		class Demo{
+			public static void main(String args[]) {
+			Animals d=new Dogs();
+			Animals c=new Cats();
+			d.dogs();
+			c.cats();
+			}
+		
+
+
+}
---
+++ 22wh1a05i6/week_days.java
@@ -0,0 +1,45 @@
+package Frame;
+import java.util.Scanner;
+public class week_days 
+{
+	    public static void main(String args[]){
+	        Scanner sc=new Scanner(System.in);
+	        System.out.println("enter the number");
+	        int n =sc.nextInt();
+	        
+	         if(n==1) 
+	         {
+	             System.out.println(" monday");
+	         }
+	        else if(n==2)
+	        {
+	        System.out.println("tuesday");
+	        }
+	        else if(n==3)
+	        {
+	        System.out.println("wednesday");
+	        }
+	        else if(n==4)
+	        {
+	        System.out.println("thursday");
+	        } 
+	        else if(n==5)
+	        {
+	        System.out.println("friday");
+	        }
+	        else if(n==6)
+	        {
+	        System.out.println("saturday");
+	        }
+	        else if(n==7)
+	        {
+	            System.out.println("sunday");
+	        }
+	        else
+	        {
+	            System.out.println(" invalid  day");
+	        }
+	}
+	
+
+}

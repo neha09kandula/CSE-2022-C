7d2fc6d317d3fcf0cdfd6b9c0c027906b72bb84a
SRI HARSHITHA INAMPUDI <140533131+harshitha0004@users.noreply.github.com>
2023-12-28 20:33:14 +0530

    question10a.java

---
+++ 22WH1A05G1/question10a.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        String s1 = "Hello, ";
+        String s2 = "world!";
+        String mergedString = s1 + s2;
+        System.out.println(mergedString);
+    }
+}

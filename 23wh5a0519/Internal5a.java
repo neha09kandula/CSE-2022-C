801fb2e69c9fd73ab9aadf6e6f80511c8f4924d9
Ruth mercy <147131989+22589java@users.noreply.github.com>
2023-12-28 23:44:57 +0530

    Add files via upload

---
+++ 23wh5a0519/Internal5a.java
@@ -0,0 +1,29 @@
+package framework;
+import java.util.*;
+class MyException extends Exception {
+		void getdata(int data) throws MyException {
+		if (data % 2 != 0) {
+		throw new MyException();
+		} else {
+		System.out.println("Even, so no exception");
+		}
+		}
+		}
+
+		public class Internal5a{
+		public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		try {
+		int n = sc.nextInt();
+		MyException e = new MyException();
+		e.getdata(n);
+		} catch (MyException me) {
+		System.out.println("It is odd -> so, raised my exception");
+
+		}
+		}
+		
+	}
+
+

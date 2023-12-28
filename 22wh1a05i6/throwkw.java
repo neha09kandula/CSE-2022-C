2306886a902738fa6ff5bbd45c50736bf6ebc254
shynitha-5i6 <147139306+shynitha-5i6@users.noreply.github.com>
2023-12-28 09:35:51 -0800

    Add files via upload

---
+++ 22wh1a05i6/throwkw.java
@@ -0,0 +1,18 @@
+class throwkw
+{
+  static void checkAge(int age)
+    {
+      if (age<18)
+      {
+          throw new ArithmeticException("Acess denied- You must be atleast 18 years old.");
+      }
+      else
+      {
+          System.out.println("Acess granted - You are old enough");
+      }
+    }
+    public  static void main(String[] args)
+    {
+        checkAge(16);
+    }
+}

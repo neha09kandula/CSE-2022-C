88acf23d53f618d92f7c638a7bce3ac70128de9d
HrithikaGundeti <147139219+HrithikaGundeti@users.noreply.github.com>
2023-12-28 22:49:23 +0530

    Add files via upload

---
+++ 22wh1a05f8/Weekday.java
@@ -0,0 +1,36 @@
+package my_java_projects;
+
+import java.util.*;
+public class Weekday {
+	  static String week(int number) {
+	        switch (number) {
+	            case 1:
+	                return "Sunday";
+	            case 2:
+	                return "Monday";
+	            case 3:
+	                return "Tuesday";
+	            case 4:
+	                return "Wednesday";
+	            case 5:
+	                return "Thursday";
+	            case 6:
+	                return "Friday";
+	            case 7:
+	                return "Saturday";
+	            default:
+	                return "Invalid day";
+	        }
+	    }
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int userNumber = scanner.nextInt();
+	        if (userNumber >= 1 && userNumber <= 7) {
+	            String weekday = week(userNumber);
+	            System.out.println( weekday);
+	        } else {
+	            System.out.println("Invalid input");
+	        }
+	    }
+}

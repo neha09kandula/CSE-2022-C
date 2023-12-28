cb25fe752b73d89c9d0728eb47861a9dfc17cd72
SRI HARSHITHA INAMPUDI <140533131+harshitha0004@users.noreply.github.com>
2023-12-28 20:35:44 +0530

    question10a.java

--- 22WH1A05G1/question10a.java
+++ 22WH1A05G1/question10a.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        String s1 = "Hello, ";
-        String s2 = "world!";
-        String mergedString = s1 + s2;
-        System.out.println(mergedString);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String s1 = scanner.nextLine();
+        System.out.print("Enter the second string: ");
+        String s2 = scanner.nextLine();
+        String res = s1 + s2;
+        System.out.println("Merged String: " + res);
     }
 }

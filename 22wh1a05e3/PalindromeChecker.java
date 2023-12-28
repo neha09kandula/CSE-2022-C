b6b154ebccd92601b5b74bea88801bf5c709db43
AksharaGaddam <147080566+AksharaGaddam@users.noreply.github.com>
2023-12-28 22:12:04 +0530

    Create PalindromeChecker.java

---
+++ 22wh1a05e3/PalindromeChecker.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+
+        boolean isPalindrome = checkPalindrome(inputString);
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + inputString + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + inputString + "\" is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    static boolean checkPalindrome(String str) {
+        str = str.replaceAll("\\s", "").toLowerCase();
+
+        int length = str.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

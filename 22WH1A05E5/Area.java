93acc1d8c9a26d8aa8867d2db244cdebc9ebe12f
Swathi Vallabhaneni <139073074+swathiv22@users.noreply.github.com>
2023-12-28 20:36:51 +0530

    Add files via upload

--- 22WH1A05E5/Area.java
+++ 22WH1A05E5/Area.java
@@ -1,30 +1,33 @@
-package internal;
-import java.util.*;
+package swathi;
+interface Shape{
+	int a;
+	int b;
+	void printArea();
+}
+class Rectangle implements Shape{
+	 public void printArea() {
+	 System.out.println("l*b");
+}
+}
+class Triangle implements Shape{
+	public void printArea() {
+		System.out.println("1/2 *b*h");
+	}
+}
+class Circle implements Shape{
+	public void printArea() {
+		System.out.println("3.14*r*r");
+	}
+}
 public class Area {
-	public float carea(float r)
-	{
-		return  (float) (3.14*r*r);
+	public static void main(String [] args) {
+		Shape obj=new Rectangle();
+		Shape obj1=new Triangle();
+		Shape obj2=new Circle();
+		obj1.printArea();
+		obj.printArea();
+		obj2.printArea();
 		
-	}
-	public float rarea(float l,float w)
-	{
-		return l*w;
-	}
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the raidus of the circle");
-		float radius=input.nextFloat();
-		System.out.println("Enter the length and width of the rectangle");
-		float length = input.nextFloat();
-		float width=input.nextFloat();
-		Area obj = new Area();
-		float r1 = obj.carea(radius);
-		float r2= obj.rarea(length, width);
-		System.out.println("The area of the circle is");
-		System.out.println(r1);
-		System.out.println("The area of the rectangle is");
-		System.out.println(r2);
-		input.close();
-	}
+}
 }

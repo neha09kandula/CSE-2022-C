60b9a784206d010942edc379dcdf701b7d0d138c
Deekshitha2705 <147140658+Deekshitha2705@users.noreply.github.com>
2023-12-29 13:59:29 +0530

    Add files via upload

---
+++ 22WH1A05E4/Throw.java
@@ -0,0 +1,15 @@
+package cse225e4;
+public class Throw {
+	static void checkAge(int age) {
+		if (age < 18) {
+		throw new ArithmeticException(" NO Access - You must be at least 18 years old.");
+		}
+		else {
+		System.out.println("Access granted - You are old enough!");
+		}
+		}
+		public static void main(String[] args) {
+		checkAge(19); 
+		}
+     }
+

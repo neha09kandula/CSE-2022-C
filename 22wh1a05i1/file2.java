98b964ca2f4c1ec88bc00cf745141a66dea6b4f4
pragnapadamata_i1 <147139262+pragnapadamata@users.noreply.github.com>
2023-12-29 15:07:50 +0530

    Add files via upload

---
+++ 22wh1a05i1/file2.java
@@ -0,0 +1,23 @@
+package frames;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class file2 {
+public static void main(String[] args) {
+try {
+File Obj = new File("myfile.txt");
+Scanner Reader = new Scanner(Obj);
+while (Reader.hasNextLine()) {
+String data = Reader.nextLine();
+System.out.println(data);
+}
+Reader.close();
+}
+catch (FileNotFoundException e) {
+System.out.println(" error occurred.");
+e.printStackTrace();
+}
+
+}
+
+}

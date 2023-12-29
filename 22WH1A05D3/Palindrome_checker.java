f6277cbf187480b11e1da606cf325745877eebda
22wh1a05d3 <147804868+22wh1a05d3@users.noreply.github.com>
2023-12-29 13:28:31 +0530

    Add files via upload

---
+++ 22WH1A05D3/Palindrome_checker.java
@@ -0,0 +1,21 @@
+package cse225d3;
+
+public class Palindrome_checker {
+    public static void main(String[] args) {
+        String input = "LEVEL";  
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+    public static boolean isPalindrome(String str) {
+        int length = str.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

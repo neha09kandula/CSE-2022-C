a297a058766138d25612e28e970318c15fa2aef0
nehadatla <147139017+neha5d6@users.noreply.github.com>
2023-12-29 15:25:52 +0530

    Add files via upload

---
+++ 22WH1A05D6/file_delete.java
@@ -0,0 +1,18 @@
+
+import java.io.File;
+
+public class file_delete {
+	public static void main(String[] args) {
+		File Obj = new
+		File("myfile.txt");
+			if (Obj.delete()) {
+
+				System.out.println("The deleted file is : "+ Obj.getName());
+			}
+			else {
+
+				System.out.println("Failed in deleting the file.");
+			}
+	}
+
+}

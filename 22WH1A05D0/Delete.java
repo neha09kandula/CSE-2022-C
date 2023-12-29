3cf5e4aaf971c579b4e9db34bd6c7c21e19f0f60
smritibandaru5d0 <147139116+smritibandaru5d0@users.noreply.github.com>
2023-12-29 15:48:24 +0530

    Create Delete.java

---
+++ 22WH1A05D0/Delete.java
@@ -0,0 +1,11 @@
+public class Delete {
+public static void main(String [] args) {
+           File obj = new File("myfile.txt");
+           if(obj.delete()) {
+                  System.out.println("File has been deleted");
+            }
+            else {
+                  System.out.println("There seems to be an error.");
+            }
+     }
+}

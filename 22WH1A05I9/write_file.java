d9fce73c8cb2e753b4e14f89ca65d5640403974e
22wh1a05i9ankam <147139333+22wh1a05i9ankam@users.noreply.github.com>
2023-12-29 13:44:17 +0530

    Add files via upload

---
+++ 22WH1A05I9/write_file.java
@@ -0,0 +1,19 @@
+package cse225i9;
+import java.io.FileWriter;
+import java.io.IOException;
+public class write_file {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileWriter Writer= new FileWriter("myfile.txt");
+			Writer.write("Files in Java are seriously good!!");
+			Writer.close();
+			System.out.println("Successfully written.");
+			}
+		catch (IOException e) {
+			System.out.println("An error has occurred.");
+			e.printStackTrace();
+			}
+		}
+
+}

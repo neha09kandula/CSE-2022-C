d1b6a967d6b02a08d8609c3b19db2e57c4fbe5bd
Pranaviveeramallu <124708954+Pranaviveeramallu@users.noreply.github.com>
2023-12-29 14:23:02 +0530

    Add files via upload

---
+++ 22wh1a05i4/Createfile.java
@@ -0,0 +1,37 @@
+package cse5i4;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Createfile {
+	public static void main(String[] args)
+	{
+		try {
+
+			File Obj = new File("myfile.txt");
+			if (Obj.createNewFile()) {
+			System.out.println("File created: "
+
+			+ Obj.getName());
+			}
+			else {
+			System.out.println("File already exists.");
+			}
+			}
+			catch (IOException e) {
+			System.out.println("An error has occurred.");
+			e.printStackTrace();
+			}
+			}
+
+			
+		
+	}
+
+
+
+
+
+
+
+

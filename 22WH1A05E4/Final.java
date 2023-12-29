93e45dd5fa833b0270db86263d9edf90d92b354e
Deekshitha2705 <147140658+Deekshitha2705@users.noreply.github.com>
2023-12-29 13:54:27 +0530

    Add files via upload

---
+++ 22WH1A05E4/Final.java
@@ -0,0 +1,21 @@
+package cse225e4;
+public class Final {
+	public static void main(String[] args) {
+		try
+		{
+		System.out.println ("::Try Block::");
+		int data = 125 / 5;
+		System.out.println ("Result:" + data);
+		}
+		
+		catch (NullPointerException e) {
+		System.out.println ("::Catch Block::");
+		System.out.println (e);
+		}
+		finally {
+		System.out.println (":: FinallyBlock::");
+		System.out.println ("NoException::finally block executed");
+		}
+		System.out.println ("rest of the code...");
+		}
+}

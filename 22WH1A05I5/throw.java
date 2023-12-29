40177b1684743dc66b1bcc5e0d324cb6707fe124
Tejaswi5i5 <147140488+Tejaswi5i5@users.noreply.github.com>
2023-12-29 15:53:16 +0530

    throw.java

---
+++ 22WH1A05I5/throw.java
@@ -0,0 +1,14 @@
+package cse225i5;
+public class Throw {
+	static void checkAge(int age) {
+		if (age < 18) {
+		throw new ArithmeticException(" NO Access - You must be at least 18 years old.");
+		}
+		else {
+		System.out.println("Access granted - You are old enough!");
+		}
+		}
+		public static void main(String[] args) {
+		checkAge(19); 
+		}
+     

4f0adc2f21e3333df9a563e87eda294b868bb134
Swathi Vallabhaneni <139073074+swathiv22@users.noreply.github.com>
2023-12-29 14:45:39 +0530

    Update Write.java

--- 22WH1A05E5/filehandling/Write.java
+++ 22WH1A05E5/filehandling/Write.java
@@ -17,34 +17,4 @@ public class Write {
 		}
 	}
 }
-package Practiceprograms;
 
-import java.io.File;
-
-public class deletefile {
-
-public static void
-main(String[] args) {
-
-// TODO Auto-generated
-
-
-
-File Obj = new
-File("myfile.txt");
-if (Obj.delete()) {
-
-System.out.println("The
-deleted file is : "
-
-+ Obj.getName());
-}
-else {
-
-System.out.println(
-"Failed in
-deleting the file.");
-}
-}
-
-}

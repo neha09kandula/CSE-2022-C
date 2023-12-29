00f356ebec43ee040c65cab8766297031560d6b4
AksharaGaddam <147080566+AksharaGaddam@users.noreply.github.com>
2023-12-29 13:56:01 +0530

    Add files via upload

---
+++ 22wh1a05e3/Throw.java
@@ -0,0 +1,15 @@
+
+package cse225e3;
+public class Throw {
+static void checkAge(int age) {
+if (age < 18) {
+throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+}
+else {
+System.out.println("Access granted - You are old enough!");
+}
+}
+public static void main(String[] args) {
+checkAge(17);
+}
+}

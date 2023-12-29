0f608d43cd25f3adabd12956ec681b6e7566cf68
Swathi Vallabhaneni <139073074+swathiv22@users.noreply.github.com>
2023-12-29 14:00:34 +0530

    Add files via upload

---
+++ 22WH1A05E5/Write.java
@@ -0,0 +1,20 @@
+package Java;
+import java.io.FileWriter;
+import java.io.IOException;
+public class Write {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileWriter obj=new FileWriter("myfile.txt");
+			obj.write("This a java text file");
+			obj.close();
+			System.out.println("Written in the file");
+		}
+		catch(IOException e) {
+			System.out.println("There seems to be an error");
+			e.printStackTrace();
+		}
+	}
+
+}

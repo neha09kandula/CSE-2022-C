b3c772895ccfa87ef169b81c495e69da45f7bc71
AksharaBhavitha <147138993+AksharaBhavitha@users.noreply.github.com>
2023-12-29 14:47:08 +0530

    Add files via upload

---
+++ 22wh1a05e0/factorial.java
@@ -0,0 +1,19 @@
+package Frame;
+import java.util.*;
+public class factorial {
+
+
+public static void main(String[] args){
+ 
+ System.out.println("enter the number: ");
+ try (Scanner obj = new Scanner(System.in)) {
+	int num=obj.nextInt();
+	 int fact=1;
+	 int i;
+	 for(i=1;i<=num;i++){
+	 fact=fact*i;
+	}
+	 System.out.println("factorial of given number is: "+fact);
+}
+}
+}

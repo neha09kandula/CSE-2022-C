8f803d691d80ea6e4bcf3a8071b5d647a0ff249a
madhavi22wh1a05e7 <147139035+madhavi22wh1a05e7@users.noreply.github.com>
2023-12-29 15:53:09 +0530

    Add files via upload

---
+++ 22WH1A05E7/trycatch.java
@@ -0,0 +1,17 @@
+package cse225e7;
+public class trycatch {
+public static void main(String[] args) {
+int val1, val2;
+try {
+System.out.println("Try Block:: Start");
+val1 = 0;
+val2 = 25 / val1;
+System.out.println(val2);
+System.out.println("Try Block:: End");
+}
+catch (ArithmeticException e){
+System.out.println("ArithmeticException:: Divide by Zero!!");
+}
+System.out.println("outside try-catch::Rest of the code.");
+}
+}

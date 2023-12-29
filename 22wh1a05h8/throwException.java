a3025c4d27e8aa7fc9ce9bddea4b7a0c30d1503a
srujana225h8 <147139729+srujana225h8@users.noreply.github.com>
2023-12-29 15:52:42 +0530

    Create throwException.java

---
+++ 22wh1a05h8/throwException.java
@@ -0,0 +1,17 @@
+package cse225h8;
+
+public class throwException {
+	public static void validate(int age) {
+		if(age<18) {
+			throw new ArithmeticException("Not eligible to vote."); 
+		}
+		else {
+			System.out.println("Eligible to vote.");
+		}
+	}
+	public static void main(String [] args) {
+		validate(15);
+		System.out.println("Rest of the code.");
+	}
+
+}

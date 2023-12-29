5ae303282c10b2c5a1da3af75b292eeb6ea1c2e5
smritibandaru5d0 <147139116+smritibandaru5d0@users.noreply.github.com>
2023-12-29 15:50:02 +0530

    Create MouseEvent.java

---
+++ 22WH1A05D0/MouseEvent.java
@@ -0,0 +1,43 @@
+public class MouseEx extends Frame implements MouseListener {
+	Label l;
+	MouseEx(){
+		addMouseListener(this);
+		l = new Label();
+		l.setBounds(20,50,100,20);
+		add(l);
+		setSize(300,300);
+		setLayout(null);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+			dispose();
+		}
+		});
+		setVisible(true);
+	}
+	public void mouseClicked(MouseEvent e) {
+		l.setText("Mouse Clicked");
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		l.setText("Mouse Pressed");
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		l.setText("Mouse Released");
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		l.setText("Mouse Enetered");
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		l.setText("Mouse Exited");
+	}
+	public static void main(String[] args) {
+		new MouseEx();
+	}
+}

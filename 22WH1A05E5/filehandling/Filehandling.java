4d63d230e5d3a46594561ca3bfdb692b029cc492
Swathi Vallabhaneni <139073074+swathiv22@users.noreply.github.com>
2023-12-29 14:23:54 +0530

    Add files via upload

---
+++ 22WH1A05E5/filehandling/Filehandling.java
@@ -0,0 +1,23 @@
+//creating a file
+package Java;
+import java.io.File;
+import java.io.IOException;
+public class Filehandling {
+	public static void main(String [] args) {
+		try {
+			File obj=new File("myfile.txt");
+			if(obj.createNewFile()) {
+				System.out.println("A file is created "+obj.getName());
+			}
+			else {
+				System.out.println("File already exists??");
+			}
+		}
+		catch(IOException e ){
+			System.out.println("An error has occured");
+			e.printStackTrace();
+		}
+		
+		
+	}
+}

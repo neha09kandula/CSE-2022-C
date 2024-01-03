052d942bf7e0806440a9a3f3b58013380d4d85a8
Ravali22wh1a05e1 <147139267+Ravali22wh1a05e1@users.noreply.github.com>
2024-01-03 22:41:08 +0530

    Add files via upload

---
+++ 22wh1a05e1/Division.java
@@ -0,0 +1,33 @@
+package ExceptionHandling;
+
+import java.util.*;
+public class Division 
+{
+	public static void main(String[] args)
+    {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of a:");
+		int a=sc.nextInt();
+		System.out.println("Enter the value of b:");
+		int b=sc.nextInt();
+		System.out.println("Enter the value of c:");
+		int c=sc.nextInt();
+		
+        int result;
+        try
+        {
+            result = a / (b - c);
+            System.out.println("result:" + result);
+        }
+ 
+        catch (ArithmeticException e)
+        {
+            System.out.println("Exception caught is DivisionByZero");
+        }
+ 
+        finally
+        {
+            System.out.println("Exception is handled");
+        }
+    }
+}

cca01a919214a03a0afe5a73a56f20b56cfe6e01
VarshiniReddy1015 <147071761+VarshiniReddy1015@users.noreply.github.com>
2024-01-04 23:44:58 +0530

    Add files via upload

---
+++ 22WHIA05E6/merge_string.java
@@ -0,0 +1,14 @@
+package java_internal_questions;
+
+public class merge_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1=new String("Varshini");
+		String s2=new String("Chaitanya");
+		System.out.println(s1+s2);
+		
+
+	}
+
+}

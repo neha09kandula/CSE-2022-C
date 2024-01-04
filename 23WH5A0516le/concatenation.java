7132342c8b983c65ad6e9f530d6e1ce6bae301e1
AMEENA23WH5A0516 <149765676+AMEENA23WH5A0516@users.noreply.github.com>
2024-01-04 20:10:50 +0530

    Add files via upload

---
+++ 23WH5A0516le/concatenation.java
@@ -0,0 +1,16 @@
+package bvrith;
+import java.util.*;
+public class concatenation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter any two strings");
+		Scanner in = new Scanner(System.in);
+		String s1=in.nextLine();
+		String s2=in.nextLine();
+		String s=s1.concat(s2);
+		System.out.println("Concatenated String: "+s);
+		
+	}
+
+}

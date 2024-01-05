c05c8dec4d6c6da36707cde1039b65871cd754c9
spoorthi225f2 <147139368+spoorthi225f2@users.noreply.github.com>
2024-01-05 15:56:30 +0530

    Add files via upload

---
+++ 22WH1A05F2/threads.java
@@ -0,0 +1,15 @@
+package cse225f2;
+
+public class threads extends Thread 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Thread t = new Thread();
+		t.start();
+		System.out.println("Thread is running");
+	}
+	public void run()
+	{
+	}
+}
---
+++ 22WH1A05F2/threads_subclass.java
@@ -0,0 +1,15 @@
+package cse225f2;
+
+public class threads_subclass extends Thread
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		threads_subclass thread = new threads_subclass();
+		thread.start();
+		System.out.println("Thread is running");
+	}
+	public void run()
+	{ 
+	}
+}

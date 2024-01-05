088955fc3dc2cef4b965fe926f9eb70bb7d3b409
VarshiniReddy1015 <147071761+VarshiniReddy1015@users.noreply.github.com>
2024-01-05 15:00:07 +0530

    Add files via upload

---
+++ 22WHIA05E6/Override.java
@@ -0,0 +1,24 @@
+package learning_java;
+
+
+class Vehicle{
+	 public void drive() {
+		System.out.println("Driving a Car");
+	}
+}
+class Car extends Vehicle{
+	public void drive() {
+		System.out.println("Repairing a car");
+	}
+	
+}
+public class Override {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car obj=new Car();
+		obj.drive();
+	}
+
+}
+
+

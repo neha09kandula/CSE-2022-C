78afc350cb6bb8f602685a18245733cbee517916
AksharaBhavitha <147138993+AksharaBhavitha@users.noreply.github.com>
2024-01-05 15:07:57 +0530

    Add files via upload

---
+++ 22wh1a05e0/Multithreads.java
@@ -0,0 +1,28 @@
+package Frame;
+
+public class Multithreads extends Thread {
+
+    public static void main(String[] args) {
+        Multithreads thread1 = new Multithreads();
+        thread1.start();
+
+        Multithreads thread2 = new Multithreads();
+        thread2.start();
+
+        try {
+            Thread.sleep(2000);
+            thread1.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run() {
+        try {
+            for (int i = 0; i < 2000; i++) {
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread is interrupted and an exception is thrown");
+        }
+    }
+}

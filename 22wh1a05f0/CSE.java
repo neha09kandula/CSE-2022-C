c700f225ec16898b8795a890b045b7754542c28b
bhukyabhavana5f0 <147141424+bhukyabhavana5f0@users.noreply.github.com>
2024-01-05 15:48:33 +0530

    Add files via upload

--- 22wh1a05f0/CSE.java
+++ 22wh1a05f0/CSE.java
@@ -1,16 +1,17 @@
 package cse225f0;
 
-public class main extends Thread
-{
+public class CSE extends Thread {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Thread t = new Thread();
-		t.start();
+		CSE Thread = new CSE();
+		Thread.start();
 		System.out.println("Thread is running inside");
 	}
 public void run()
 {
 	System.out.println("Thread is running outside");
 }
-}
+
+	}
+

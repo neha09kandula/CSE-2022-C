adc60da072d1ba563094099378b3b25934ea56fe
AksharaGaddam <147080566+AksharaGaddam@users.noreply.github.com>
2024-01-16 18:20:50 +0530

    Update declaringstrings.java

--- 22wh1a05e3/declaringstrings.java
+++ 22wh1a05e3/declaringstrings.java
@@ -3,8 +3,8 @@ package javaprograms;
 public class declaringstrings {
 
 	public static void main(String[] args) {
-		String str="Akshara";
-		System.out.println(str);
+		String str=new String("Akshara");
+		System.out.println("The string is: "+str);
 
 	}
 

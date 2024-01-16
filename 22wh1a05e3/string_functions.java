e3d7db6ef0b7bc8d0ed78802b42f5f91b45ad02f
AksharaGaddam <147080566+AksharaGaddam@users.noreply.github.com>
2024-01-16 17:57:07 +0530

    Add files via upload

---
+++ 22wh1a05e3/string_functions.java
@@ -0,0 +1,15 @@
+package java_5e3;
+
+public class string_functions {
+
+	public static void main(String[] args) {
+		int x=25;
+		System.out.println(Math.sqrt(x));
+		String name="akshara";
+		System.out.println(name.toUpperCase());
+		String s="AKSHARA";
+		System.out.println(s.toLowerCase());
+		System.out.println(s.length());
+	}
+
+}

c78d8e1a145ecbb8e380881066d85bd57d0a5a1b
AksharaGaddam <147080566+AksharaGaddam@users.noreply.github.com>
2024-01-16 10:51:50 +0530

    Add files via upload

---
+++ 22wh1a05e3/arithmatic_operations.java
@@ -0,0 +1,24 @@
+package java_5e3;
+
+public class arithmatic_operations {
+
+	public static void main(String[] args) {
+	    int a,b,c,d,e,f;
+	    a=10;
+	    b=10;
+	    c=a+b;
+	    d=a-b;
+	    e=a*b;
+	    f=a/b;
+	    
+   System.out.print("addition: ");
+   System.out.println(c);
+   System.out.print("subtraction: ");
+   System.out.println(d);
+   System.out.print("Multiplication: ");
+   System.out.println(e);
+   System.out.print("Division: ");
+   System.out.println(f);
+	}
+
+}

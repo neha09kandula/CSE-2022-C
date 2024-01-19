eb0cc034b01981059319e7bbe80e8a696fb74b94
vedasreekonka <147120250+vedasreekonka-5d7@users.noreply.github.com>
2024-01-19 15:45:59 +0530

    Create demo.java

---
+++ 22WH1A05D7/simple applet/demo.java
@@ -0,0 +1,11 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.*;
+  
+public class demo extends Applet{  
+   public void paint(Graphics g){
+        g.drawString("welcome",100,100);
+        setBackground(Color.pink);  
+}  
+  
+} 

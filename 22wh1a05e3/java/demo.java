a9c2a3370a3f30af7f753a383bd737dfb6e52209
AksharaGaddam <147080566+AksharaGaddam@users.noreply.github.com>
2024-01-19 15:57:00 +0530

    Add files via upload

---
+++ 22wh1a05e3/java/demo.java
@@ -0,0 +1,17 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class demo extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello World!",70, 100);
+        g.setColor(Color.black);
+        setBackground(Color.yellow);
+        
+    }
+
+public static void main(String[] args){
+
+}
+
+}

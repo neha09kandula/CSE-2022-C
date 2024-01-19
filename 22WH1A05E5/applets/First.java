e064c13c695f1305a6197ae1e450e7865542dc9e
Swathi Vallabhaneni <139073074+swathiv22@users.noreply.github.com>
2024-01-19 15:50:29 +0530

    Add files via upload

---
+++ 22WH1A05E5/applets/First.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;  
+import java.awt.*;  
+public class First extends Applet{  
+  
+public void paint(Graphics g){
+g.setColor(Color.black);
+setBackground(Color.yellow);
+g.drawString("My name is swathi!",150,150);
+}  
+}  

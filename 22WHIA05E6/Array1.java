3a1ed67de522516fe2431eda9bd90478f86f2bcb
VarshiniReddy1015 <147071761+VarshiniReddy1015@users.noreply.github.com>
2024-01-19 20:56:51 +0530

    Add files via upload

---
+++ 22WHIA05E6/Array1.java
@@ -0,0 +1,20 @@
+package practice_problems;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a=new int[5];
+		int i;
+		for( i=0;i<5;i++) {
+			a[i]=i;
+		}
+
+		for(i=0;i<5;i++) {
+			System.out.println(a[i]);
+		}
+
+		
+	}
+
+}

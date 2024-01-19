133bdf941518a2e57b38d285cdca6aba48b39920
akshaya15 <147140875+23wh5a0515akshaya@users.noreply.github.com>
2024-01-19 15:35:33 +0530

    Add files via upload

---
+++ 23wh5A0515/SimpleApplet.java
@@ -0,0 +1,12 @@
+import java.applet.*;
+import java.awt.*;
+
+public class SimpleApplet extends Applet
+{
+      public void paint(Graphics g)
+      {
+           g.setColor(Color.black);
+           setBackground(Color.black);
+           g.drawString("hii Applet",80,150);
+       }
+} 

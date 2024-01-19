1cc075dec9b783536431214cc9436356a169dc45
Neha-22wh1ao5i8 <147142049+Neha-22wh1ao5i8@users.noreply.github.com>
2024-01-19 15:32:15 +0530

    Add files via upload

---
+++ 22wh1a05i8/SimpleApplet.java
@@ -0,0 +1,13 @@
+import java.applet.Applet;
+import java.awt.*;
+public class SimpleApplet extends Applet
+{
+		public void paint(Graphics g)
+ 		{
+			g.drawString(" HI APPLET PROGRAM ",80,150);
+			g.setColor(Color.black);
+			setBackground(Color.yellow);
+		}
+	}
+
+

eceb1aeacf250d6179616928419df1ebdb48992b
Sravya_22wh1a05g9 <130379309+22wh1a05g9@users.noreply.github.com>
2024-01-19 16:17:06 +0530

    Create applet1.java

---
+++ 22WH1A05G6/applet1.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+
+public class MyApplet extends Applet {
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.drawString("HI APPLET PROGRAM", 80, 150);
+    }
+}

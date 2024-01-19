c4800c16d4970dc969129736fc0ca5a4af90a361
veda5d5 <140432451+veda-2081@users.noreply.github.com>
2024-01-19 14:55:46 +0530

    Add files via upload

--- 22WH1A05D5/factorial.java
+++ 22WH1A05D5/factorial.java
@@ -1,18 +1,40 @@
-package frames;
-import java.util.*;
-public class factorial {
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class factorial extends Applet implements ActionListener {
+    TextField t1,t2;
+    Button a;
+    Label l1,l2;
 
-public static void main(String[] args){
- 
- System.out.println("enter the number: ");
- Scanner obj=new Scanner(System.in);
- int num=obj.nextInt();
- int fact=1;
- int i;
- for(i=1;i<=num;i++){
- fact=fact*i;
+    public void init() {
+        
+        a = new Button("Compute");
+	a.addActionListener(this);
+        t1=new TextField(20);
+        t2=new TextField(20);	
+	l1=new Label("number");
+	l2=new Label("result");
+	
+	add(l1);
+	add(l2);
+	add(t1);
+	add(t2);
+	add(a);
+	
 }
- System.out.println("factorial of given number is: "+fact);
+public void actionPerformed(ActionEvent e){
+if(e.getSource()==a)
+{
+int a = Integer.parseInt(t1.getText());
+int fact=1;
+for(int i = 1;i<=a;i++)
+fact*=i;
+t2.setText(""+fact);
+}
+else{
+t1.setText("");
+t2.setText("");
+}
 }
 }
